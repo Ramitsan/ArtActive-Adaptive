22a4ce47f03b730122b6fb6c2989f64cbfaed7dc
Ramitsan <ramitsan@gmail.com>
2020-07-06 18:09:33 +0300

    Добавляет функции открытия и закрытия модального окна и оверлея

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,35 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+//Показ и закрытие модального окна
+var headerCallButton = document.querySelector('.page-header__call-button'); //ссылка в хэдере
+var modalRequestCall = document.querySelector('.request-call'); //модальное окно "Закажите звонок"
+var modalOverlay = document.querySelector('.modal-overlay');
+
+
+var modalRequestCallShow = function() {
+    modalRequestCall.classList.add('modal--show');
+};
+
+var modalOverlayShow = function() {
+    modalOverlay.classList.add('modal--show');
+};
+
+headerCallButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalRequestCallShow();
+    modalOverlayShow();
 });
