d78158dace6749582cbb1d19569a496b4153b0c7
Ramitsan <ramitsan@gmail.com>
2020-07-06 18:39:17 +0300

    Добавляет закрытие модального окна и оверлея по кнопке-крестику

--- build/js/script.js
+++ build/js/script.js
@@ -17,6 +17,7 @@
 //Показ и закрытие модального окна
 var headerCallButton = document.querySelector('.page-header__call-button'); //ссылка в хэдере
 var modalRequestCall = document.querySelector('.request-call'); //модальное окно "Закажите звонок"
+var modalCloseRequestCallButton = modalRequestCall.querySelector('.modal-close'); //кнопка закрытия
 var modalOverlay = document.querySelector('.modal-overlay');
 
 
@@ -24,8 +25,16 @@ var modalRequestCallShow = function() {
     modalRequestCall.classList.add('modal--show');
 };
 
+var modalRequestCallClose = function() {
+    modalRequestCall.classList.remove('modal--show');
+};
+
 var modalOverlayShow = function() {
     modalOverlay.classList.add('modal--show');
+};
+
+var modalOverlayClose = function() {
+    modalOverlay.classList.remove('modal--show');
 };
 
 headerCallButton.addEventListener('click', function(evt) {
@@ -33,3 +42,9 @@ headerCallButton.addEventListener('click', function(evt) {
     modalRequestCallShow();
     modalOverlayShow();
 });
+
+modalCloseRequestCallButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalRequestCallClose();
+    modalOverlayClose();
+})
