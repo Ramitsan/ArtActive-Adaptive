d274586e9ea48b34e76595130091db0e3422900c
Ramitsan <ramitsan@gmail.com>
2020-09-02 20:31:44 +0300

    Добавляет бургерное меню в планшетной версии

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,5 @@
 'use strict';
-var mainNav = document.querySelector('.main-nav');
-var mainNavToggle = document.querySelector('.main-nav__toggle');
 
-mainNav.classList.remove('page-header--nojs');
-
-mainNavToggle.addEventListener('click', function() {
-    if (mainNav.classList.contains('main-nav--closed')) {
-        mainNav.classList.remove('main-nav--closed');
-        mainNav.classList.add('main-nav--opened');
-    } else {
-        mainNav.classList.add('main-nav--closed');
-        mainNav.classList.remove('main-nav--opened');
-    }
-});
 
 //Показ и закрытие модального окна
 var mainNavCallButton = document.querySelector('.main-nav__call-button'); //ссылка в хэдере
@@ -24,90 +11,90 @@ var ESC_KEYCODE = 27;
 
 
 var modalRequestCallShow = function() {
-    modalRequestCall.classList.add('modal--show');
+  modalRequestCall.classList.add('modal--show');
 };
 
 var modalRequestCallClose = function() {
-    modalRequestCall.classList.remove('modal--show');
+  modalRequestCall.classList.remove('modal--show');
 };
 
 var modalOverlayShow = function() {
-    modalOverlay.classList.add('modal--show');
+  modalOverlay.classList.add('modal--show');
 };
 
 var modalOverlayClose = function() {
-    modalOverlay.classList.remove('modal--show');
+  modalOverlay.classList.remove('modal--show');
 };
 
 var disableScroll = function() {
-    document.body.classList.add('body-scroll');
+  document.body.classList.add('body-scroll');
 }
 
 var activateScroll = function() {
-    document.body.classList.remove('body-scroll');
+  document.body.classList.remove('body-scroll');
 }
 
 var overlayClickHandler = function(popup) {
-    modalOverlay.addEventListener('click', function() {
-        popup.classList.remove('modal--show');
-        modalOverlayClose();
-        activateScroll();
-    })
+  modalOverlay.addEventListener('click', function() {
+    popup.classList.remove('modal--show');
+    modalOverlayClose();
+    activateScroll();
+  })
 }
 
 overlayClickHandler(modalRequestCall);
 
 
 mainNavCallButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalRequestCallShow();
-    modalOverlayShow();
-    disableScroll();
+  evt.preventDefault();
+  modalRequestCallShow();
+  modalOverlayShow();
+  disableScroll();
 });
 
 modalCloseRequestCallButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalRequestCallClose();
-    modalOverlayClose();
-    activateScroll();
+  evt.preventDefault();
+  modalRequestCallClose();
+  modalOverlayClose();
+  activateScroll();
 })
 
 bannersPrintConsultationLink.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalRequestCallShow();
-    modalOverlayShow();
-    disableScroll();
+  evt.preventDefault();
+  modalRequestCallShow();
+  modalOverlayShow();
+  disableScroll();
 })
 
 window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-        if (modalRequestCall.classList.contains('modal--show')) {
-            evt.preventDefault();
-            modalRequestCallClose();
-            modalOverlayClose();
-            activateScroll();
-        }
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (modalRequestCall.classList.contains('modal--show')) {
+      evt.preventDefault();
+      modalRequestCallClose();
+      modalOverlayClose();
+      activateScroll();
     }
+  }
 });
 
 //валидация телефонного номера
 jQuery(function($) {
-    $("#poligraphy-form__user-tel").mask("+7 (999) 99 99 99");
-    $("#faq-consultation__user-tel").mask("+7 (999) 99 99 99");
-    $("#request-call__user-tel").mask("+7 (999) 99 99 99");
+  $("#poligraphy-form__user-tel").mask("+7 (999) 99 99 99");
+  $("#faq-consultation__user-tel").mask("+7 (999) 99 99 99");
+  $("#request-call__user-tel").mask("+7 (999) 99 99 99");
 });
 
 //кнопка прокрутки
 var upButton = document.querySelector('.btn--up');
 
 window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 650) {
-        upButton.classList.add('modal--show');
-    } else {
-        upButton.classList.remove('modal--show');
-    }
+  if (window.pageYOffset > 650) {
+    upButton.classList.add('modal--show');
+  } else {
+    upButton.classList.remove('modal--show');
+  }
 });
 
 upButton.addEventListener('click', function() {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 });
